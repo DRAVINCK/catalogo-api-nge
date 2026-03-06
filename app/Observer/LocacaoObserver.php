26c7666a9403846bdc4aec9ed6962a87b73e97f0
Lucas Teixeira <teixeirajob.one@gmail.com>
2026-03-05 21:08:42 -0300

    feat: add novas key para limpar no cache durante processos

--- app/Observer/LocacaoObserver.php
+++ app/Observer/LocacaoObserver.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Cache;
 class LocacaoObserver{
     private function clearCache(){
         Cache::forget('locacoes');
+
+        Cache::forget('livros');
+        Cache::forget('total_locacoes');
+        Cache::forget('relatorio:mais_locados');
     }
 
     public function created(Locacao $locacao){
